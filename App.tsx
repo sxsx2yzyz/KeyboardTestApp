c68782e756b5b5e90417447498215dbb6b62811c
yangzhen <yangzhen@systech.com.cn>
2025-07-18 09:56:05 +0800

    feat: enhance InputModal with animated keyboard handling and dynamic layout adjustments

--- App.tsx
+++ App.tsx
@@ -5,107 +5,137 @@
  * @format
  */
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import {
   Button,
   Dimensions,
   Keyboard,
+  StatusBar,
   Text,
   TextInput,
+  useWindowDimensions,
   View,
 } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { KeyboardAvoidingView, KeyboardEvents, KeyboardProvider, useResizeMode } from 'react-native-keyboard-controller';
 import { createModalStack, modalfy, ModalProvider } from 'react-native-modalfy';
+import Animated, { useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
 
 
-const InputModal = () => {
-  const [RNKeyboardEventHeight, setRNKeyboardEventHeight] = useState<number | null>(null);
-  const [KeyboardEventsWillShowHeight, setKeyboardEventsWillShowHeight] = useState<number | null>(null);
-  const [KeyboardEventsDidShowHeight, setKeyboardEventsDidShowHeight] = useState<number | null>(null);
+const InputModal = ({ modal: { closeModal, getParam } }) => {
+  const [KeyboardEventsWillShowHeight, setKeyboardEventsWillShowHeight] = useState<number>(0);
+  const [bottomOfComponent, setBottomOfComponent] = useState<number>(0);
+  const { height: windowHeight } = useWindowDimensions();
+  const { height: screenHeight } = Dimensions.get('screen');
+  const containerRef = useRef<View>(null);
+  const marginKeyboard = 100;
+  const translateY = Math.min(0, screenHeight - KeyboardEventsWillShowHeight - bottomOfComponent - marginKeyboard);
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{ translateY: withTiming(translateY, { duration: 200 }) }],
+  }));
+
+  const onLayout = () => {
+    containerRef.current?.measure((x, y, width, height, pageX, pageY) => {
+      setBottomOfComponent(pageY + height);
+    });
+  };
+  const keyboardType = getParam('keyboardType');
   useResizeMode();
   useEffect(() => {
-    const keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', (e) => {
-      setRNKeyboardEventHeight(e.endCoordinates.height);
-    });
-    const keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', () => {
-      setRNKeyboardEventHeight(0);
-    });
     const keyboardEventsWillShowListener = KeyboardEvents.addListener('keyboardWillShow', (e) => {
       setKeyboardEventsWillShowHeight(e.height);
     });
     const keyboardEventsWillHideListener = KeyboardEvents.addListener('keyboardWillHide', () => {
       setKeyboardEventsWillShowHeight(0);
     });
-    const keyboardEventsDidShowListener = KeyboardEvents.addListener('keyboardDidShow', (e) => {
-      setKeyboardEventsDidShowHeight(e.height);
-    });
-    const keyboardEventsDidHideListener = KeyboardEvents.addListener('keyboardDidHide', () => {
-      setKeyboardEventsDidShowHeight(0);
-    });
     return () => {
-      keyboardDidShowListener.remove();
-      keyboardDidHideListener.remove();
       keyboardEventsWillShowListener.remove();
       keyboardEventsWillHideListener.remove();
-      keyboardEventsDidShowListener.remove();
-      keyboardEventsDidHideListener.remove();
     }
   }, []);
 
   return (
-    <KeyboardAvoidingView
-      behavior="padding"
-    style={{
-      width: Dimensions.get('window').width,
-      height: 400,
-      backgroundColor: '#fff',
-      borderRadius: 16,
-      padding: 20,
-      overflow: 'hidden',
-    }}>
-      <Text>RNKeyboardEventHeight: {RNKeyboardEventHeight}</Text>
-      <Text>KeyboardEventsWillShowHeight: {KeyboardEventsWillShowHeight}</Text>
-      <Text>KeyboardEventsDidShowHeight: {KeyboardEventsDidShowHeight}</Text>
-      <TextInput style={{
-        height: 40,
-        borderColor: 'gray',
-        borderWidth: 1,
-      }}
-        placeholder='Enter your name'
-        placeholderTextColor='gray'
-        autoFocus={true}
-        keyboardType="default"
-        returnKeyType="done"
-        secureTextEntry={false}
-        value='not avoid keyboard'
-      />
-      <Button title="Close" onPress={() => {
-        modalfy().closeModal();
-      }} />
-    </KeyboardAvoidingView>
+    <>
+      <Animated.View
+        ref={containerRef}
+        onLayout={onLayout}
+        style={[{
+          width: Dimensions.get('window').width,
+          backgroundColor: '#fff',
+          borderRadius: 16,
+          padding: 20,
+        }, animatedStyle]}>
+        <Text>KeyboardHeight: {KeyboardEventsWillShowHeight}</Text>
+        <Text>BottomOfComponent: {bottomOfComponent}</Text>
+        <Text>translateY: {Math.min(0, windowHeight - (KeyboardEventsWillShowHeight || 0) - bottomOfComponent)}</Text>
+        <Text>windowHeight: {windowHeight}</Text>
+        <Text>screenHeight: {screenHeight}</Text>
+        <Text>marginKeyboard: {marginKeyboard}</Text>
+        <TextInput style={{
+          height: 40,
+          borderColor: 'gray',
+          borderWidth: 1,
+        }}
+          placeholder='Enter your name'
+          placeholderTextColor='gray'
+          autoFocus={true}
+          keyboardType={keyboardType}
+          returnKeyType="done"
+          secureTextEntry={false}
+        // value='not avoid keyboard'
+        />
+        <Button title="Close" onPress={() => {
+          modalfy().closeModal();
+        }} />
+
+      </Animated.View>
+      <Animated.View style={[{
+        height: marginKeyboard,
+        width: '100%',
+        borderBottomColor: "red",
+        borderBottomWidth: 5,
+      }, animatedStyle]}>
+
+      </Animated.View>
+    </>
   )
 }
 
 const stack = createModalStack({
   InputModal: {
     modal: InputModal,
-    position: "center"
+    backBehavior: 'none',
   },
 }, {
+  backdropOpacity: 0.6,
+  backdropAnimationDuration: 200,
+  disableFlingGesture: true,
+  stackContainerStyle: {
+    zIndex: 2000,
+  },
 });
 function App(): React.JSX.Element {
   return (
-    <GestureHandlerRootView>
-      <KeyboardProvider>
+    <GestureHandlerRootView style={{flex: 1, backgroundColor: '#fff'}}>
+      <KeyboardProvider
+        statusBarTranslucent={true}
+        navigationBarTranslucent={true}
+      >
         <ModalProvider stack={stack}>
           <View style={{
             flex: 1,
             justifyContent: 'center',
             alignItems: 'center',
           }}>
-            <Button title="Open Modal" onPress={() => {
-              modalfy().openModal('InputModal');
+            <Button title="Open number-pad Modal" onPress={() => {
+              modalfy().openModal('InputModal', {
+                keyboardType: 'numeric'
+              });
+            }} />
+            <Button title="Open default Modal" onPress={() => {
+              modalfy().openModal('InputModal', {
+                keyboardType: 'visible-password'
+              });
             }} />
           </View>
           {/* <KeyboardAvoidingView behavior="padding">
